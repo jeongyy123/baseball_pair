3d52351f7834e5a040db41b0261c988c97d0a726
jeongyy123 <jeongyy123@kakao.com>
2023-10-19 21:32:16 +0900

    random three number 업데이트(+2)

--- homework.js
+++ homework.js
@@ -1,37 +1,53 @@
 const prompt = require("prompt-sync")({ sigint: true });
+var _ = require('lodash');
 
-let arr = ""
-let randomNum = function () {
-    return Math.floor((Math.random() * 10))
-};
+//making random three number-1 :string
+// let arr = ""
+
+// for (let i = 0; i < 3; i++) {
+//     let b = Math.floor((Math.random() * 10));
+//     if (!arr.includes(b)) {
+//         arr += b
+//     } else {
+//         i--;
+//         continue;
+//     };
+// };
+
+// //making random three number-2 :set
+// var arr = new Set();
+
+// while( arr.size < 3) {
+//     let b = ~~((Math.random() * 10));
+//     arr.add(b)
+// }
 
 
-for (let i = 0; i < 3; i++) {
-    let b = randomNum();
-    if (!arr.includes(b)) {
-        arr += b
-    } else {
-        i--;
-        continue;
-    };
-};
+//making random three number-2 :lodash 라이브러리 사용
+let arr = "";
+while (arr.length < 3){
+    let b = _.random(0, 9);
+    if (!arr.includes(b)){
+        arr += b;
+    }
+}
 
-
-let input_user = ""
+// get user input number
+let input_user = "";
 let count =0;
-// input, 유효성 검사
+
 function get_input_number() {
     count++;
     input_user = prompt("숫자 3개를 입력해주세요")
 
     if (input_user.match(/\D/)) {
-        console.log("3자리의 수만 입력해 주세요")
+        console.log("❗숫자만 입력해 주세요")
         return get_input_number()
     } else if (input_user.length !== 3) {
-        console.log("세자리 숫자만 넣어주세요")
+        console.log("❗세자리 숫자만 넣어주세요")
         return get_input_number()
     } else if (input_user[0] === input_user[1] || input_user[0] === input_user[2] || input_user[1] === input_user[2]){
-        console.log("중복되지 않는 3자리 숫자를 넣어주세요")
+        console.log("❗중복되지 않는 3자리 숫자를 넣어주세요")
         return get_input_number()
     } else {
         playball()
@@ -44,11 +60,10 @@ function get_input_number() {
 // strike, ball 판단
 
 
-
 let strike = 0; 
 let ball = 0;
 
-
+//strike
 function strike_count(){
 for (let i=0; i<arr.length; i++) {
     if (input_user[i] === arr[i]) {
@@ -67,7 +82,7 @@ function playball() {
         console.log(`${count}번만에 맞히셨습니다`)
         return console.log("게임을 종료합니다")
     } else {
-        console.log(`strike: ${strike}, ball: ${ball}`)
+        console.log(`${strike}S${ball}B`)
         strike = 0
         ball = 0
         get_input_number()
